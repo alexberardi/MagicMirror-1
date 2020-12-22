b9b2ecbab1874f669c68b67ac38c282675f41f4e
Alex Berardi <alexmberardi@gmail.com>
2020-12-22 16:10:34 -0500

    Minor changes after first successful deploy.

--- config/config.js
+++ config/config.js
@@ -60,7 +60,7 @@ var config = {
 			config: {
 				location: "New York",
 				locationID: "",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-				appid: "YOUR_OPENWEATHER_API_KEY"
+				appid: "6d3cc3a632aaa6c93edd758ea9f36de9"
 			}
 		},
 		{
@@ -70,7 +70,7 @@ var config = {
 			config: {
 				location: "New York",
 				locationID: "5128581",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-				appid: "YOUR_OPENWEATHER_API_KEY"
+				appid: "6d3cc3a632aaa6c93edd758ea9f36de9"
 			}
 		},
 		{
